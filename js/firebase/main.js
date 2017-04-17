cb2302308f7b65993b12279308231d7c8a30f529
jeegar298 <jeegarjam@gmail.com>
2017-04-16 21:30:22 -0400

    Release 2.0 Code added

--- js/firebase/main.js
+++ js/firebase/main.js
@@ -4,12 +4,19 @@ function showRestaurants() {
   var dbRef = firebase.database().ref().child("Restaurants").child('1').child('Items');
   window.alert('lol');
   dbRef.on('value', function(datasnapshot) {
-
-  datasnapshot.forEach(function(childSnapshot) {
-  var childData = childSnapshot.val();
-  window.alert(childData.Name);
-});
-
+    datasnapshot.forEach(function(childSnapshot) {
+    var childData = childSnapshot.val();
+    window.alert(childData.Name);
+    });
   });
-    
+}
+
+function getCartCountAndDisplay() {
+  var userid = 1;
+  var cartcount = document.getElementById("cartcount");
+  var cartRef = firebase.database().ref("Users/"+userid).child("Cart");
+  cartRef.on("value", function (snapShot) {
+    cartcount.innerHTML = snapShot.numChildren();
+  });
+   
 }
